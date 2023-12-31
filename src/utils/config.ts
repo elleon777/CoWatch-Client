69fb0bac935c04eaf441e365d9942719b7f3c11a
elleon777 <60982813+elleon777@users.noreply.github.com>
2023-12-31 20:42:46 +0300

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,1 +1,1 @@
-export const WEB_SOCKET_HOST = process.env.VITE_WEB_SOCKET_HOST || 'http://localhost:9999';
+export const WEB_SOCKET_HOST = import.meta.env.VITE_WEB_SOCKET_HOST || 'http://localhost:9999';
